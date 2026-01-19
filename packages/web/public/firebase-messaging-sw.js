18f27256006ff97fb88c417a69e4c05320d499a7
vinitghaturle <vinitghaturle656@gmail.com>
2026-01-19 21:22:04 +0530

    fixed

--- packages/web/public/firebase-messaging-sw.js
+++ packages/web/public/firebase-messaging-sw.js
@@ -5,14 +5,15 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
 
 // Firebase config - must match your app config
-firebase.initializeApp({
-    apiKey: "AIzaSyAJqxSv4MeGox4ZtiTChKRhb5CdbRJd9m8",
-    authDomain: "skill-swap-7c51b.firebaseapp.com",
-    projectId: "skill-swap-7c51b",
-    storageBucket: "skill-swap-7c51b.firebasestorage.app",
-    messagingSenderId: "725300348404",
-    appId: "1:725300348404:web:1955a49c56992a837d25a1"
-});
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
 const messaging = firebase.messaging();
 
